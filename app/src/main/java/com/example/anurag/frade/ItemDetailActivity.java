2db8e2e5e8203cf6b7e62e0d9c9fc7f44ed6daf3
Anurag <anurag.smishra@gmail.com>
2015-12-15 18:15:33 +0530

    hi

--- app/src/main/java/com/example/anurag/frade/ItemDetailActivity.java
+++ app/src/main/java/com/example/anurag/frade/ItemDetailActivity.java
@@ -30,7 +30,7 @@ public class ItemDetailActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own detail action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own detail action", Snackbar.LENGTH_SHORT)
                         .setAction("Action", null).show();
             }
         });
